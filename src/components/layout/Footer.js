f58d080b4717a1444cf4f9c151482268d5e46d97
Breno Leal <brenoooleal@gmail.com>
2024-01-24 11:31:23 -0300

    Footer Atualizado

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,11 +1,12 @@
 import styles from './Footer.module.css'
-import {FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import {FaFacebook, FaInstagram, FaLinkedin, FaGithub} from 'react-icons/fa'
 const Footer = () => {
     return ( 
         <footer  className={styles.footer}>
             <ul className={styles.social_list}>
                 <li><FaFacebook/></li>
                 <li><FaInstagram/></li>
+                <li><FaGithub/></li>
                 <li><FaLinkedin/></li>
             </ul>
             <p className={styles.copy_right}><span>Costs</span> &copy; 2024</p>

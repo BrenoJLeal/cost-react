30ec7d956802ef16e6a5a6137c86b985fedc5e8a
Breno Leal <brenoooleal@gmail.com>
2024-01-24 10:48:58 -0300

    Layout inicial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Contato from './components/pages/Contato';
+import NewProject from './components/pages/NewProject';
+import Company from './components/pages/Company';
 
+import Container from './components/layout/Container';
 
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
+    <Router>
+      {/* Inicio dos Links */}
+      <ul>
+        <Link to='/'>Home</Link>
+        <Link to='/contact'>Contato</Link>
+        <Link to='/company'>Company</Link>
+        <Link to='/newproject'>Novo projeto</Link>
+      </ul>
+      {/* Fim dos Links */}
+      
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/company" element={<Company/>}/>
+          <Route exact path='/contact' element={<Contato/>}/>
+          <Route exact path="/newproject" element={<NewProject/>}/>
+        </Routes>
+      </Container>
 
-    </div>
+      <p>footer</p>
+    </Router>
   );
 }
 

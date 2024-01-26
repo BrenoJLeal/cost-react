c26e3f32045422b9d2982b0186e4e1145c4180c6
BrenoJLeal <140741784+BrenoJLeal@users.noreply.github.com>
2024-01-25 22:19:07 -0300

    Update ProjectForm.js

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -8,7 +8,7 @@ const ProjectForm = ({handleSubmit, projectData, btnText}) => {
     const [project, setProject] = useState(projectData || {});
 
     useEffect(() =>{
-        fetch('http://localhost:5000/categories',
+        fetch('https://cost-server-kappa.vercel.app/categories',
         {
             method:"GET",
             headers: {
